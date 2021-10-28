bfe54ac0f407f9892eeb19b4a0bcaac871b3865a
JoãoCamargo <joaoclaudiocamargo@gmail.com>
2021-10-28 16:35:30 -0300

    master

--- heranca/1-heranca.php
+++ heranca/1-heranca.php
@@ -17,4 +17,23 @@ class Documento {
 
 }
 
+// Herança
+class CPF extends Documento { 
+
+    public function validar():bool{
+        
+        $numeroCPF = $this->getNumero();
+        
+        // Validação do CPF
+        return true;
+    }
+
+}
+
+$doc = new CPF();
+$doc->setNumero('33445567889-66');
+$doc->validar();
+var_dump($doc->validar());
+echo "\n";
+echo $doc->getNumero();
 ?>
